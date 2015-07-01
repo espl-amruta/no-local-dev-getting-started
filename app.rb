77aa733be46b0f5c30cfc30bd126d92c17bb15e2
espl-amruta <amrutakumbhakarn@gmail.com>
2015-07-01 15:57:46 +0530

    Update app.rb

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class Contact < ActiveRecord::Base
 end
 
 get "/contacts" do
-  @contacts = Contact.order(:lastname)
+  @contacts = Contact.where(:email => "rinkuamru123@gmail.com")
   erb :index
 end
 
